92a716832e51f6fe1317789434e3fa37b46ce684
Pradip Yadav <pradipyadav630@gmail.com>
2026-05-13 12:10:50 +0530

    Day 11:Solved ques5

---
+++ DAY-11/practice5.ts
@@ -0,0 +1,9 @@
+type MarginValue = `${number}px` | `${number}rem` | `${number}vh`;
+
+let margin1: MarginValue = "10px";
+let margin2: MarginValue = "2rem";
+let margin3: MarginValue = "50vh";
+
+console.log(margin1);
+console.log(margin2);
+console.log(margin3);
