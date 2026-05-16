4dbd1615fd1b642d1ee70d5c8f5b4098f54ddef7
Pradip Yadav <pradipyadav630@gmail.com>
2026-05-16 12:35:07 +0530

    Day-14 add full-page login validation screenshot and update code

--- DAY-14/playwright.config.js
+++ DAY-14/playwright.config.js
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
+import path from 'path';
 
 /**
  * Read environment variables from file.
@@ -31,6 +32,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
   },
 
   /* Configure projects for major browsers */
@@ -72,10 +74,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    cwd: `${path.resolve(process.cwd())}/webapp`,
+    command: 'node index.js',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+  },
 });
-
